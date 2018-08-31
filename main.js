57a9262e7ced80e8e96dcd6ca07bdbb1ee86bdbe
Vivian Huang <vivianhhuang@gmail.com>
2018-08-31 11:49:52 -0700

    Adding Events page with screening info

--- main.js
+++ main.js
@@ -8,6 +8,9 @@ $(function() {
   $("#about").click(function() {
     $("#content-container").load("about.html");
   });
+  $("#events").click(function() {
+    $("#content-container").load("events.html");
+  });
   $("#contact").click(function() {
     $("#content-container").load("contact.html");
   });
