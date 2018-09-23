c11bcd2e001f6b4c090c264ccb03a76cf277a48e
Vivian Huang <vivianhhuang@gmail.com>
2018-09-23 15:36:18 -0700

    Adding cast & crew bios, episode tab template, update contact info.

--- main.js
+++ main.js
@@ -1,16 +1,24 @@
 $(function() {
   // Default to about page
   $(document).ready(function() {
-    $("#content-container").load("about.html");
+    $("#content-container").load("episodes.html");
   });
 
   // Set up navbar links
   $("#about").click(function() {
     $("#content-container").load("about.html");
   });
+  $("#episodes").click(function() {
+    $("#content-container").load("episodes.html");
+  });
+  $("#cast").click(function() {
+    $("#content-container").load("cast.html");
+  });
+  /*
   $("#events").click(function() {
     $("#content-container").load("events.html");
   });
+  */
   $("#contact").click(function() {
     $("#content-container").load("contact.html");
   });
